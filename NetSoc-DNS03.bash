e86ecc54afae3eeef46adbf91839aff6ab35ee63
Unknown <Kingr4@protonmail.ch>
2018-09-08 03:08:46 +0100

    Added NetSoc-DNS03

--- NetSoc-DNS03.bash
+++ NetSoc-DNS03.bash
@@ -1,8 +1,8 @@
 virt-install \
 --network bridge:br0 \
---name NetSoc-DNS01 \
+--name NetSoc-DNS03 \
 --ram=512 \
 --vcpus=1 \
---disk path=/home/vm/storage/NetSoc-DNS01.img,size=10 \
+--disk path=/home/vm/storage/NetSoc-DNS03.img,size=10 \
 --cdrom /home/vm/images/ubuntu-16.04.5-server-amd64.iso \
 --graphics vnc
